324fb1daef5704dd584ca1c4067618922c99f73e
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-03-06 21:43:50 +0200

    Adding unit testing for button and userProfile components

---
+++ src/components/button/Button.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Button from "./Button";
+
+describe("Button Component", () => {
+  const click = jest.fn();
+  test("should be rendered with Next text", () => {
+    render(<Button text="Next" isDisabled={true} onClick={click} />);
+
+    const btnElement = screen.getByText("Next");
+    expect(btnElement).toBeInTheDocument();
+  });
+
+  test("should be rendered with Prev text", () => {
+    render(<Button text="Prev" isDisabled={true} onClick={click} />);
+
+    const btnElement = screen.getByText("Prev");
+    expect(btnElement).toBeInTheDocument();
+  });
+
+  test("should be disabled", () => {
+    render(<Button text="Prev" isDisabled={true} onClick={click} />);
+
+    const btnElement = screen.getByText("Prev");
+    expect(btnElement).toBeDisabled();
+  });
+
+  test("should not be disabled", () => {
+    render(<Button text="Prev" isDisabled={false} onClick={click} />);
+
+    const btnElement = screen.getByText("Prev");
+    expect(btnElement).not.toBeDisabled();
+  });
+
+  test("should have button--disabled class if it is disabled", () => {
+    render(<Button text="Prev" isDisabled={true} onClick={click} />);
+
+    const btnElement = screen.getByText("Prev");
+    expect(btnElement).toHaveClass("button--disabled");
+  });
+
+  test("should fire click event", () => {
+    render(<Button text="Next" isDisabled={false} onClick={click} />);
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    expect(click).toHaveBeenCalled();
+  });
+});
---
+++ src/components/user-profile/UserProfile.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import IUser from "interfaces/User";
+import UserProfile from "./UserProfile";
+
+describe("UserProfile Component", () => {
+  const user: IUser = {
+    name: {
+      first: "Alaa",
+      last: "Ahmad",
+    },
+    picture: {
+      large: "https://via.placeholder.com/140x100",
+      medium: "https://via.placeholder.com/140x100",
+      thumbnail: "https://via.placeholder.com/140x100",
+    },
+  };
+  test("render userProfile component", () => {
+    const { container } = render(<UserProfile user={user} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render user image", () => {
+    render(<UserProfile user={user} />);
+
+    const img = screen.getByRole("img");
+    expect(img).toHaveAttribute("src", user.picture.medium);
+  });
+
+  it("should render user name", () => {
+    render(<UserProfile user={user} />);
+
+    const pElm = screen.getByText("Alaa Ahmad");
+    expect(pElm).toBeInTheDocument();
+  });
+});
